b4c9c95cb6a71a4833f9cf0cdad812dc6879501e
Antonyes222 <97543582+Antonyes222@users.noreply.github.com>
2026-01-12 15:01:14 -0800

    Update About.tsx

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,6 @@
 export function About() {
     return (
-        <p>Hello. I am anton and this is my popup cards website. I have created this because I have once created popup cards a long time ago, but I abandoned the hobby. This is my way of continuing the passion while being able to put my programming skills to use.</p>
+        <p>Hello. I am anton and this is my popup cards website. I used to create popup cards a long time ago, but I abandoned the hobby. This is my way of continuing the passion while being able to put my programming skills to use.</p>
     )
 }
 
